05f5f2dfc822050cef9a308ec93c01a426acfee4
Supphanat Deawwanich <90847280+Sxphx@users.noreply.github.com>
2025-03-25 15:16:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -16,10 +16,13 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN || "https://rad-marigold-8b05b4.netlify.app", 
-    credentials: true,
+    origin: "https://rad-marigold-8b05b4.netlify.app",
+    credentials: true, 
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
 
 app.use(
   session({
